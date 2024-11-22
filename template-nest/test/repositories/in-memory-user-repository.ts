e1367f3434279628465f725076013e51e253da3c
barretot <barretoruan77@gmail.com>
2024-11-22 10:56:23 -0300

    first commit

--- template-nest/test/repositories/in-memory-user-repository.ts
+++ template-nest/test/repositories/in-memory-user-repository.ts
@@ -6,7 +6,7 @@ import { UserRepository } from '@/domain/repositories/user/UserRepository'
 import { InMemoryDatabaseService } from './in-memory-database.service'
 
 @Injectable()
-export class UserRepositoryAdapter implements UserRepository {
+export class InMemoryUserRepository implements UserRepository {
   constructor(private db: InMemoryDatabaseService<User>) {}
 
   async findByEmail(email: string): Promise<User | null> {
