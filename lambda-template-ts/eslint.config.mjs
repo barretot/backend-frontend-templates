77d1fc62de5ce7d661a1757748a39fc112032636
Ruan <barretoruan77@gmail.com>
2025-02-12 17:27:39 -0300

    fix lambda

--- lambda-template-ts/eslint.config.mjs
+++ lambda-template-ts/eslint.config.mjs
@@ -1,23 +1,22 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import typescriptParser from '@typescript-eslint/parser'
-import importPlugin from 'eslint-plugin-import'
-import nPlugin from 'eslint-plugin-n'
-import prettierPlugin from 'eslint-plugin-prettier'
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import importPlugin from "eslint-plugin-import";
+import nPlugin from "eslint-plugin-n";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
   {
-    files: ['src/**/*.ts', 'test/**/*.ts'],
-    ignores: ['node_modules/**', 'dist/**'],
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    ignores: ["node_modules/**", "dist/**", "eslint.config.mjs"],
     languageOptions: {
       parser: typescriptParser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: './tsconfig.json',
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
     },
     plugins: {
-      '@typescript-eslint': typescriptEslint,
+      "@typescript-eslint": typescriptEslint,
       import: importPlugin,
       prettier: prettierPlugin,
       n: nPlugin,
@@ -27,44 +26,48 @@ export default [
       ...typescriptEslint.configs.recommended.rules,
 
       // Regras do Prettier diretamente no ESLint
-      'prettier/prettier': [
-        'error',
+      "prettier/prettier": [
+        "error",
         {
           printWidth: 80,
           tabWidth: 2,
           singleQuote: true,
-          trailingComma: 'all',
-          arrowParens: 'always',
-          semi: false,
+          trailingComma: "all",
+          arrowParens: "always",
+          semi: true,
         },
       ],
 
       // Regras de importação
-      'import/order': [
-        'error',
+      "import/order": [
+        "error",
         {
           groups: [
-            ['builtin', 'external'],
-            ['internal'],
-            ['parent', 'sibling', 'index'],
+            ["builtin", "external"],
+            ["internal"],
+            ["parent", "sibling", "index"],
           ],
-          'newlines-between': 'always',
+          "newlines-between": "always",
           alphabetize: {
-            order: 'asc',
+            order: "asc",
             caseInsensitive: true,
           },
         },
       ],
-      'import/no-duplicates': 'error',
-      'import/no-unresolved': 'error',
-      'import/newline-after-import': ['error', { count: 1 }],
-      quotes: ['error', 'single', { avoidEscape: true }],
-      semi: ['error', 'never'],
+
+      "@typescript-eslint/no-explicit-any": "warn",
+      "import/no-duplicates": "error",
+      "import/no-unresolved": "error",
+      "import/newline-after-import": ["error", { count: 1 }],
+      quotes: ["error", "single", { avoidEscape: true }],
+      semi: ["error", "always"],
     },
     settings: {
-      'import/resolver': {
-        typescript: {},
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+        },
       },
     },
   },
-]
+];
