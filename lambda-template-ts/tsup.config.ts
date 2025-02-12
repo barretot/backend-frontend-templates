7b6309673917dc7bf73e85ff1421a03362845168
Ruan <barretoruan77@gmail.com>
2025-02-12 17:01:42 -0300

    fix lambda

--- lambda-template-ts/tsup.config.ts
+++ lambda-template-ts/tsup.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  outDir: 'build',
-  format: ['esm'],
-  target: 'node22',
-  sourcemap: true, 
-  clean: true, 
-  splitting: false, 
-  bundle: true, 
-})
+  outDir: 'dist',
+  format: ['esm'], 
+  target: 'esnext',
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+});
